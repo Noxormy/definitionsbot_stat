842983eb15db38c0ecb22506121400ce29965a38
Daniil Pshegorlinskii <noxormy@gmail.com>
2022-02-20 17:39:06 +0300

    Updates

--- src/helpers/configs.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyA3L0MLIy9lt6-hRXn2-IrXSp0-gN9xjgg",
-    authDomain: "definitionsbot-4f039.firebaseapp.com",
-    projectId: "definitionsbot-4f039",
-    storageBucket: "definitionsbot-4f039.appspot.com",
-    messagingSenderId: "443576261082",
-    appId: "1:443576261082:web:212f497bb7190613e4c8e3"
-};
